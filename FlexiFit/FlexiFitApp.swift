5ed9d1bc6adabbcd6b0b5129b16801ba96fd952d
Vini Patel <vinip@uci.edu>
2024-01-31 17:41:29 -0800

    New Project

---
+++ FlexiFit/FlexiFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlexiFitApp.swift
+//  FlexiFit
+//
+//  Created by Vini Patel on 1/31/24.
+//
+
+import SwiftUI
+
+@main
+struct FlexiFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
