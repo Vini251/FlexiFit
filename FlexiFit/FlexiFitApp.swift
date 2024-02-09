9022623d0b42ddb6fbbe2b7efe5f5402ba13072e
Vini Patel <80379653+Vini251@users.noreply.github.com>
2024-02-08 18:49:58 -0800

    Update FlexiFitApp.swift

--- FlexiFit/FlexiFitApp.swift
+++ FlexiFit/FlexiFitApp.swift
@@ -6,11 +6,15 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
 
 @main
 struct FlexiFitApp: App {
     @State private var isLoggedIn = false
-    
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     var body: some Scene {
         WindowGroup {
             if isLoggedIn {
